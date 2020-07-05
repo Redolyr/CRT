dd25b973a4fb7c66f27d6758767d8b34b826e8e5
Redolyr <hyresann1221@hotmail.co.jp>
2020-07-06 01:52:08 +0900

    Update Test.java

--- crt/Test.java
+++ crt/Test.java
@@ -1,5 +1,7 @@
 package crt;
 
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -21,9 +23,14 @@ public class Test {
          *         x = -10/8 * 1/(1-0.8) = -50/8<br>
          *             x = -6.25
          */
-        Numeric fraction1 = new Fraction(6.25 + 1 / 0.8);
+        Numeric fraction1 = new Fraction(-6.25 + 1 / 0.8);
 
         System.out.println(fraction.equals(fraction0) + ", " + fraction.doubleValue() + " ," + fraction0.doubleValue());
-        System.out.println(fraction1);
+        System.out.println(fraction1.division(new Numeric(-6.25)).doubleValue());
+
+        System.out.println(Arrays.toString(Fraction.continuedFraction(6.25)));
+        System.out.println(Arrays.toString(Fraction.continuedFraction(3.75)));
+        System.out.println("Multiply Division " + fraction.multiply(fraction0) + ", " + fraction.division(fraction0));
+        System.out.println("Multiply Division " + fraction.multiply(fraction0).doubleValue() + ", " + fraction.division(fraction0).doubleValue());
     }
 }
