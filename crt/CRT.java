53cca64fb03adc3141978adcedf66bfa2737c259
Redolyr <hyresann1221@hotmail.co.jp>
2020-08-15 03:18:28 +0900

    Create CRT.java

---
+++ crt/CRT.java
@@ -0,0 +1,126 @@
+package crt;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class CRT {
+
+    public static final Numeric ONE = new Numeric(1);
+
+    private Vector baseNumber;
+
+    private Vector product;
+
+    private Numeric max;
+
+    public Vector getModulo(Numeric input) {
+        return clipC(this.baseNumber, input, this.max);
+    }
+
+    public Vector setBaseModulo(Vector baseNumber, Numeric input) {
+        this.baseNumber = baseNumber;
+        this.max = Vector.allProduct(this.baseNumber);
+        this.product = clip(baseNumber, input, this.max);
+        return this.getModulo(input);
+    }
+
+    public Numeric getOutput(Vector baseModulo) {
+        return clipOutput(this.baseNumber.state, baseModulo.state, this.product.state, this.max);
+    }
+
+    public Vector getBaseNumber() {
+        return this.baseNumber;
+    }
+
+    public Numeric getMax() {
+        return this.max;
+    }
+
+    private static Vector clip(Vector vector2, Numeric input, Numeric max) {
+        Numeric[] numerics;
+
+        //c
+        numerics = new Numeric[vector2.state.length];
+        for (int len = 0; len < numerics.length; ++len) numerics[len] = input.modulo(vector2.state[len]);
+        Vector vector4 = new Vector(numerics);
+
+        //export
+        numerics = new Numeric[vector2.state.length];
+        Arrays.fill(numerics, new Numeric(0));
+        Vector export = new Vector(numerics);
+
+        //a
+        numerics = new Numeric[vector2.state.length];
+        Arrays.fill(numerics, new Numeric(1));
+        Vector vector7 = new Vector(numerics);
+        for (int __len = 0; __len < vector7.state.length; ++__len)
+            for (int len = 0; len < vector7.state.length; ++len)
+                vector7.state[__len] = vector7.state[__len].multiply(len == __len ? ONE : vector2.state[len]);
+
+        Vector[] vector6 = clipBLine(vector2.state, clipAtB(vector2.state, max), max);
+
+        for (int _len = 0; _len < vector6.length; ++_len) {
+            numerics = new Numeric[vector2.state.length];
+            Arrays.fill(numerics, new Numeric(0));
+
+            Numeric f = new Numeric(0);
+            for (int __len = 0; __len < vector2.state.length; ++__len) {
+                numerics[__len] = vector6[_len].state[__len].multiply(vector7.state[__len]).multiply(vector4.state[__len]).modulo(vector2.state[__len]);
+                f = f.add(vector6[_len].state[__len].multiply(vector7.state[__len]));
+            }
+            if (!f.modulo(max).equals(ONE)) continue;
+            export = vector6[_len];
+        }
+        return export;
+    }
+
+    private static Vector[] clipBLine(Numeric[] vector2, Numeric[][] vector8, Numeric MAX) {
+        Vector[] vector6 = new Vector[0];
+        Numeric[] numerics;
+        int[] ints = new int[vector8.length];
+        long max = IntStream.range(0, vector8.length).mapToLong(len -> (ints[len] = vector8[len].length)).reduce(1, (a, b) -> a * b);
+
+        for (int len = 0; len < max; ++len) {
+            numerics = new Numeric[vector8.length];
+            vector6 = Arrays.copyOf(vector6, vector6.length + 1);
+            vector6[vector6.length - 1] = new Vector(numerics);
+            Numeric numeric = new Numeric(0);
+            for (int _len = 0; _len < vector8.length; ++_len) {
+                numerics[_len] = vector8[_len][len % ints[_len]];
+                numeric = numeric.add(numerics[_len].multiply(MAX.division(vector2[_len])));
+            }
+            numeric = numeric.modulo(MAX);
+            if (numeric.toValue().value != 1) vector6 = Arrays.copyOf(vector6, vector6.length - 1);
+        }
+        return vector6;
+    }
+
+    private static Numeric[][] clipAtB(Numeric[] vector2, Numeric max) {
+        Numeric[][] numerics = new Numeric[vector2.length][];
+        for (int _len = 0; _len < numerics.length; ++_len) {
+            Numeric numeric1 = vector2[_len];
+            for (Numeric numeric = ONE; numeric.compareTo(max.division(numeric1)) < 0; numeric = numeric.add(ONE)) {
+                if (max.division(numeric1).multiply(numeric).modulo(numeric1).equals(ONE)) {
+                    if (numerics[_len] == null) numerics[_len] = new Numeric[0];
+                    numerics[_len] = Arrays.copyOf(numerics[_len], numerics[_len].length + 1);
+                    numerics[_len][numerics[_len].length - 1] = numeric;
+                    continue;
+                }
+            }
+        }
+        return numerics;
+    }
+
+    private static Numeric clipOutput(Numeric[] vector2, Numeric[] vector9, Numeric[] vector10, Numeric max) {
+        Numeric numeric5 = new Numeric(0);
+        for (int len = 0; len < vector2.length; ++len)
+            numeric5 = numeric5.add(max.division(vector2[len]).multiply(vector9[len]).multiply(vector10[len]));
+        return numeric5.modulo(max);
+    }
+
+    private static Vector clipC(Vector vector2, Numeric numeric4, Numeric max) {
+        Numeric[] numerics2 = new Numeric[vector2.state.length];
+        for (int len = 0; len < numerics2.length; ++len) numerics2[len] = numeric4.modulo(vector2.state[len]);
+        return new Vector(numerics2);
+    }
+}
